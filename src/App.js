e38df1423f55285cea1e8f8a16fb8b069ff43072
10Sadique <jafarsjahan@gmail.com>
2022-12-07 04:04:47 +0600

    label added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import ProductForm from './components/ProductForm';
-import AddProduct from './components/AddProduct';
 import { Toaster } from 'react-hot-toast';
+import AddProduct from './components/AddProduct';
+import Labels from './components/Labels';
+import ShowForm from './components/ShowForm';
 
 function App() {
     return (
         <div className="flex items-center justify-center my-20 flex-col">
+            {/* Add product */}
             <h1 className="mb-5 text-xl font-bold text-gray-700">
                 Add Product
             </h1>
             <AddProduct />
+
+            {/* Display product */}
             <h1 className="mb-5 text-xl font-bold text-gray-700">
                 Product List
             </h1>
-            <ProductForm />
+            <Labels />
+            <ShowForm />
 
             {/* Toast component */}
             <Toaster position="top-center" reverseOrder={false} />
